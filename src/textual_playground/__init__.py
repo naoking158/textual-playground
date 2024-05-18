0a36536454f4d17d2b70e00e628b1b84e93bd50d
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2024-05-18 18:30:02 +0900

    stopwatch1

--- src/textual_playground/__init__.py
+++ src/textual_playground/__init__.py
@@ -1,3 +1,21 @@
-def main() -> int:
-    print("Hello from textual-playground!")
-    return 0
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer
+
+class StopwatchApp(App):
+    """A Textual app to manage stopwatches."""
+
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.dark = not self.dark
+
+
+def main():
+    app = StopwatchApp()
+    app.run()
