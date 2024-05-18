896c08e14560e96c738ad83dea84e33539e6e796
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2024-05-18 21:37:33 +0900

    stopwatch6

--- src/textual_playground/__init__.py
+++ src/textual_playground/__init__.py
@@ -14,14 +14,15 @@ class TimeDisplay(Static):
 
     start_time = reactive(monotonic)
     time = reactive(0.0)
+    total = reactive(0.0)
 
     def on_mount(self) -> None:
         """Event handler called when widget is adde to the app."""
-        self.set_interval(1/60, self.update_time)
+        self.update_timer = self.set_interval(1 / 60, self.update_time, pause=True)
 
     def update_time(self) -> None:
         """Method to update the time to the current time."""
-        self.time = monotonic() - self.start_time
+        self.time = self.total + (monotonic() - self.start_time)
 
     def watch_time(self, time: float) -> None:
         """Called when the time attribute changes."""
@@ -29,17 +30,38 @@ class TimeDisplay(Static):
         hours, minutes = divmod(minutes, 60)
         self.update(f"{hours:02.0f}:{minutes:02.0f}:{seconds:05.2f}")
 
+    def start(self) -> None:
+        """Method to start (or resume) time updating."""
+        self.start_time = monotonic()
+        self.update_timer.resume()
+
+    def stop(self) -> None:
+        """Method to stop the time display updating."""
+        self.update_timer.pause()
+        self.total += monotonic() - self.start_time
+        self.time = self.total
+
+    def reset(self) -> None:
+        """Method to reset ther time display to zero."""
+        self.total = 0
+        self.time = 0
+
 
 class Stopwatch(Static):
     """A stopwatch widget."""
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         """Event handler called when a button is pressed."""
-
+        button_id = event.button.id
+        time_display = self.query_one(TimeDisplay)
         if event.button.id == "start":
+            time_display.start()
             self.add_class("started")
         elif event.button.id == "stop":
+            time_display.stop()
             self.remove_class("started")
+        elif event.button.id == "reset":
+            time_display.reset()
 
     def compose(self) -> ComposeResult:
         """Create child widgets of a stopwatch."""
